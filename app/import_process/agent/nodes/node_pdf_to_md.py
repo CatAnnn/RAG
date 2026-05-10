c61ffd8e5e4c0cfa8b4bc005cfe73509944e6938
CatAnnn <2252387@mail.dhu.edu.cn>
2026-05-10 16:12:34 +0800

    更新 PDF 转 Markdown 节点

--- app/import_process/agent/nodes/node_pdf_to_md.py
+++ app/import_process/agent/nodes/node_pdf_to_md.py
@@ -13,6 +13,14 @@ def step_1_validate_paths(state):
     pass
 
 
+def step_2_upload_and_poll(pdf_path_obj):
+    pass
+
+
+def step_3_download_and_extract(zip_url, local_dir_obj, stem):
+    pass
+
+
 def node_pdf_to_md(state: ImportGraphState) -> ImportGraphState:
     """
     节点: PDF转Markdown (node_pdf_to_md)
@@ -36,6 +44,24 @@ def node_pdf_to_md(state: ImportGraphState) -> ImportGraphState:
         # 返回：校验后的文件和输出文件夹Path对象
         pdf_path_obj,local_dir_obj = step_1_validate_paths(state)
         # 3. 调用mineru 进行pdf的解析（local_file_path"原始输入文件地址"）返回一个下载文件的地址 xx.zip url地址
-        # 参数：yao
-    logger.info(f">>> [Stub] 执行节点: {sys._getframe().f_code.co_name}")
-    return state
+        # 参数：要解析的pdf文件路径    返回值：要下载的zip文件地址
+        zip_url = step_2_upload_and_poll(pdf_path_obj)
+        # 4.下载zip包，并且解析和提取 （local_dir）
+        # 参数：1.要下载的地址 2. local_dir_obj 解压的文件夹  3. 文件名 二狗子 (二狗子.pdf)
+        # 返回值：解压后md文件的真实路径
+        md_path = step_3_download_and_extract(zip_url, local_dir_obj, pdf_path_obj.stem)
+        #  5. 把md_path地址进行赋值，读取md的文件内容 md_content赋值（文本内容）
+        #  更新数据
+        state['md_path'] = md_path
+        state['local_dir'] = local_dir_obj #主要处理下！是str类型
+        # md的内容读取，配置给md_content
+        with open(md_path, 'r', encoding='utf-8') as f:
+            state['md_content'] = f.read()
+    except Exception as e:
+        # 处理异常
+        logger.error(f">>> [{function_name}]使用minerU解析发生了异常，异常信息：{e}")
+        raise # 终止工作流
+    finally:
+        # 6. 结束的日志和任务状态的配置
+        logger.info(f">>> [{function_name}]开始结束了！现在的状态为：{state}")
+        add_done_task(state['task_id'], function_name)
